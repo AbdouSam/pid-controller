a94289fad5cdfabe236190e332c26db88c985c58
AbdouSam <abdou.sam7@gmail.com>
2019-09-06 23:54:38 +0100

    Put pid struct and functions prototype.

--- pid_ctrl.h
+++ pid_ctrl.h
@@ -1,14 +1,41 @@
+#ifndef _PID_CTRL_H
+#define _PID_CTRL_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define FLOAT_EPSILON 0.00001f
+
 typedef struct 
 {
-  float kp;
-  float ki; 
-  float kd; 
-  float maint; /* Maintain value */
-  float st; /* Sampling time */
-};
+  float   kp;           /* Proportional*/
+  float   ki;           /* ki = kp *T / Ti  */
+  float   kd;           /* kd = kp * Td / T */
+  float   Ti;           /* Integral */     
+  float   Td;           /* Derivetive */     
+  float   maintain = 0; /* Maintain value */
+  float   T;            /* Sampling time */
+  float   set_point;    /* Set point */
+  bool    direct = false;
 
+  float   *in;          /* PID input or System Output */
+  float   *out;         /* PID output or System input */
+  
+  float   max_out;      /* PID maximum output */
+  float   min_out;      /* PID minimum output */
 
-pid_control()
+  float (*in_conv)(float);  /* Sensor equation adc to unit */
+  float (*out_conv)(float); /* convert to dac or pwm */
+}pid_t;
+
+typedef struct
 {
   
-}
+}zig_param_t;
+
+void pid_calculate_ki_kd(pid_t *pid);
+void pid_init(pid_t *pid);
+void pid_control(pid_t *pid);
+void pid_zigler_nicols_identification(zig_param_t *param);
+
+#endif
