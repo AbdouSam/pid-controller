ea9906b2eb8c4f7542e63b443bef024a545d2092
AbdouSam <abdou.sam7@gmail.com>
2019-09-07 18:37:46 +0100

    Update the test to work with latest change

--- pid_test.c
+++ pid_test.c
@@ -1,62 +1,110 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-volatile static bool pid_flag = false;
+#include "pid_ctrl.h"
 
-INTERRUPT_TIMER_Vector_Routine()
+/* PID Control heating system using LM35V and BTA08-600
+ * with heating element.
+ * the system is modeled as G(s) = Y(s) / U(s).
+ * Y(s) : system output in °C (temperature)
+ * U(s) : system input (command) in V (0V - 5V ) 
+ *
+ * The PID parameters and calcultaed on the basis of the system
+ * units PID is modeled as R(s) = U(s) / (W(s) - Y(s))
+ * W(s) is the set point measured in °C
+ */
+
+/* The input is LM35 1 °C for 10mV */
+float ambien_temp = 20;
+
+
+float input_conversion(float analog_input)
 {
-  pid_flag = true;
+  /* 10bit ADC with 5.0V reference */
+
+  float volts = analog_input * 5.0f / 1023.0f;
+
+  /*  LM35 :  100mV (0.1V) for 1 °C */
+
+  float temperature = volts * 1.0f / 0.01 ; 
+  
+  return analog_input;
+}
+
+float output_conversion(float output)
+{
+  /*  8bit DAC with 5V logic */
+  uint16_t analog_output = output * 255 / 5.0;
+
+  return analog_output;
+} 
+
+uint16_t analogRead(int pin)
+{
+  return ambien_temp;
+}
+
+void analogWrite(int pin, int output)
+{
+
 }
 
 int main(int argc, char const *argv[])
 {
-	/* Using Zigler nichols on a heating system we have
-   * Unit step response gives
-   * K = 50 °C
-   * T_dead = 30 s
-   * T_stdy = 230 s
-   * 
-   */
+
   float analog_temp;
   float pwm_heater;
 
-  pid_t pid_0 = 
+  int sensorPin     = 1;
+  int actuatorPin   = 2;
+  
+  pid_ctrl_t pid_0 = 
   {
-    14.5f,         /* kp */
-    2.4f,          /* ki */
-    43.5f,         /* kd */
+    0.15f,         /* kp */
+    0.01f,          /* ki */
+    0.1f,         /* kd */
     0,             /* Ti, no need since we have ki calculated */
     0,             /* Td, no need since we have kd calculated */
     0,             /* maintain = 0 */
     5.0f,          /* Sampling time 5 second */
     30.0,          /* Set point 30 °C */
     false,         /* Reverse mode*/
-    &analog_temp,  /* pointer to sensor input */
-    &pwm_heater,   /* pointer to output */
-    10.0f,         /* max 10.0V */
-    0.0f,          /* min 0.0V */
-    NULL;          /* No conversion needed in input */
-    NULL;          /* No conversion needed in output */
+    {
+      5.0f,               /* max 10.0V */
+      0.0f,                /* min 0.0V */
+      &input_conversion,   /* No conversion needed in input */
+      &output_conversion,  /* No conversion needed in output */
+    },
+    0,
+    0,
   };
 
-  adc_setup(CHANNEL_0);
-  dac_setup(PWM_0);
-  timer_setup(TIMER0, pid_0.T);
-  
   while(true)
   {
 
-    if(pid_flag)
+    printf("Getting Temperature\n");
+    analog_temp = analogRead(sensorPin); 
+
+    printf("Temperature = %d \n", (int)analog_temp );
+
+    pwm_heater = pid_control(&pid_0, (float)analog_temp);
+
+    printf("PID output = %f\n",pwm_heater );
+
+    analogWrite(actuatorPin, pwm_heater);
+
+    for(int i = 0; i < 5000000; i++)
     {
-      pid_flag = false;
 
-      adc_read(CHANNEL_0, &analog_temp);
+    }
+    ambien_temp+=0.5;
 
-      pid_ctrl(pid_0)
-
-      dac_write(PWM_0, pwm_heater);
+    if(ambien_temp > pid_0.spt + 2)
+    {
+      break;
     }
   }
-  
 	return 0;
 }
